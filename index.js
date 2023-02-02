61bb2b5f098e323b529497bba08c02a07a4b4e1d
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-02-02 22:44:20 +0100

    update part add

--- index.js
+++ index.js
@@ -74,6 +74,23 @@ async function run() {
 
 
 
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await orderCollection.updateOne(query, updateDoc);
+            res.send(result);
+        })
+
+        
+
+
+
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
